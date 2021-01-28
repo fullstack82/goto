6ffadadf948cb5338e302a383055c91759f8bf50
fullstack82 <fullstack8210@gmail.com>
2021-01-28 23:45:16 +0100

    creacion de marca

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // GOTO
-goto marca;     // goto busca la marca, se salta lo que hay entre medias y directamente ejecuta lo que hay detras de la marca;
+goto ejecuta_aqui;     // goto busca la marca, se salta lo que hay entre medias y directamente ejecuta lo que hay detras de la marca;
 echo "<h3>Instruccion 1</h3>";
 echo "<h3>Instruccion 2</h3>";
 echo "<h3>Instruccion 3</h3>";
@@ -9,5 +9,8 @@ echo "<h3>Instruccion 4</h3>";
 echo "<h3>Instruccion 5</h3>";
 
 
-marca:          // Marca que busca el goto
-    echo "<h3>Me he saltado 5 echo</h3>";
+ejecuta_aqui:          // Marca que busca el goto, la marca puede ser cualquier nombre.
+    echo "<h3>Me he saltado 2 echo</h3>";
+
+
+    
